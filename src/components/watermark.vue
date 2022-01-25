eb0b976b922c0a94c1ac82a9dc48957026f64a70
softwareycl <693812981@qq.com>
2022-01-25 09:49:56 +0800

    优化
    优化

--- src/components/watermark.vue
+++ src/components/watermark.vue
@@ -2,22 +2,30 @@
  * @Author: ycl
  * @Date: 2022-01-19 19:58:28
  * @LastEditors: ycl
- * @LastEditTime: 2022-01-24 11:14:58
+ * @LastEditTime: 2022-01-25 09:49:10
  * @Description: 水印组件
 -->
 
 <template>
-  <div
-    ref="wm"
-    class="wm-container"
-  ></div>
+  <div ref="wm" class="wm-container"></div>
 </template>
 <script>
 import tool from "../utils/tool";
 export default {
   mounted() {
     //获取水印背景图
-    this.wmBackground = tool.addWaterMark("your name");
+    const option1 = {
+      strContent: "yourname",
+      x: "0",
+      y: "100",
+    };
+    const option2 = {
+      strContent: "yourname",
+      x: "150",
+      y: "200",
+    };
+    this.wmBackground1 = tool.addWaterMark(option1);
+    this.wmBackground2 = tool.addWaterMark(option2);
 
     //将水印添加到节点样式
     const wmDom = this.$refs["wm"];
@@ -28,7 +36,7 @@ export default {
             height:100vh;
             z-index:9999;
             pointer-events:none;
-            background-image:url('${this.wmBackground}')`;
+            background-image:url('${this.wmBackground1}'),url('${this.wmBackground2}')`;
     wmDom.setAttribute("style", styleStr);
     //防删操作
     const options = {
@@ -37,33 +45,34 @@ export default {
       subtree: true,
     };
     const observer = new MutationObserver(() => {
-      console.log('监听中');
+      console.log("监听中");
       const wmInstance = document.querySelector(".wm-container");
 
-        //如果标签在，只修改了属性，重新赋值属性
-        if (wmInstance) {
-          if (wmInstance.getAttribute("id")) {
-            wmInstance.removeAttribute("id");
-          }
-          if (wmInstance.getAttribute("class") !== "wm-container") {
-            wmInstance.setAttribute("class", "wm-container");
-          }
-          if(wmInstance.getAttribute("style") !== styleStr) {
-            wmInstance.setAttribute("style", styleStr);
-          }
-        } else {
-          if(wmDom.getAttribute('class') == 'wm-container') {
-            pNode.insertBefore(wmDom, pNode.firstChild);
-          } else {
-            wmDom.setAttribute('class', 'wm-container')
-          }
+      //如果标签在，只修改了属性，重新赋值属性
+      if (wmInstance) {
+        if (wmInstance.getAttribute("id")) {
+          wmInstance.removeAttribute("id");
         }
+        if (wmInstance.getAttribute("class") !== "wm-container") {
+          wmInstance.setAttribute("class", "wm-container");
+        }
+        if (wmInstance.getAttribute("style") !== styleStr) {
+          wmInstance.setAttribute("style", styleStr);
+        }
+      } else {
+        if (wmDom.getAttribute("class") == "wm-container") {
+          pNode.insertBefore(wmDom, pNode.firstChild);
+        } else {
+          wmDom.setAttribute("class", "wm-container");
+        }
+      }
     });
     observer.observe(pNode, options);
   },
   data() {
     return {
-      wmBackground: "",
+      wmBackground1: "",
+      wmBackground2: "",
     };
   },
 };
--- src/utils/tool.js
+++ src/utils/tool.js
@@ -2,19 +2,19 @@
  * @Author: ycl
  * @Date: 2022-01-19 15:32:26
  * @LastEditors: ycl
- * @LastEditTime: 2022-01-24 11:15:09
+ * @LastEditTime: 2022-01-25 09:48:59
  * @Description: 工具类
  */
 
 export default {
     //添加水印
-    addWaterMark(name) {
+    addWaterMark(options) {
         let curDate = new Date();
         let formatDate = curDate.getFullYear() + '/' + curDate.getMonth() + 1 + '/' + curDate.getDate()
-        const strArr = `${name}|${formatDate}`;
+        const strArr = `${options.strContent}|${formatDate}`;
         let ctx = document.createElement("canvas");
-        ctx.width = 200;
-        ctx.height = 100;
+        ctx.width = 400;
+        ctx.height = 200;
         ctx.style.display = "none";
         let cans = ctx.getContext("2d");
         cans.rotate((-20 * Math.PI) / 180);
@@ -22,7 +22,7 @@ export default {
         cans.fillStyle = "rgba(17, 17, 17, 0.20)";
         cans.textAlign = "left";
         cans.textBaseline = "Middle";
-        cans.fillText(strArr, 0, 100);
+        cans.fillText(strArr, options.x, options.y);
         cans.save();
         return ctx.toDataURL();
     }
